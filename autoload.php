9afe78891b54f8141b999b31f7ee5ed5c145c7b9
gabrielmacus <gabrielmacus@gmail.com>
2018-06-22 20:35:51 -0300

    ...

---
+++ autoload.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Puers
+ * Date: 22/06/2018
+ * Time: 20:32
+ */
+
+
+$envs = ["production","development"];
+
+foreach ($envs as $env):
+    $path=__DIR__."/env/{$env}.php";
+    if(file_exists($path)):
+        include($path);
+    endif;
+endforeach;
